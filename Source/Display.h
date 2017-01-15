9a484c80c1fe7b4ed139890694e185bae6fa4cb9
Mark A. Ramage <mark.a.ramage@vanderbilt.edu>
2017-01-15 00:24:50 -0600

    First commit

---
+++ Source/Display.h
@@ -0,0 +1,19 @@
+#ifndef DISPLAY_H_INCLUDED
+#define DISPLAY_H_INCLUDED
+
+namespace Display
+{
+    void init();
+
+    void close();
+
+    void clear();
+
+    void update();
+
+    void checkForClose();
+
+    bool isOpen();
+}
+
+#endif // DISPLAY_H_INCLUDED
